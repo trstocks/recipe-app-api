dcc88f468ee3e762d58cdf0ffc4ed7314bfe7368
Troy Stocks <tstocks@ceratopsdev.com>
2019-09-16 16:35:46 -0400

    Configured postgres db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -5,19 +5,21 @@ from django.db.utils import OperationalError
 from django.test import TestCase
 
 
-class CommandTests(TestCase):
+class CommandsTestCase(TestCase):
 
     def test_wait_for_db_ready(self):
         """Test waiting for db when db is available"""
+
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.return_value = True
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 1)
 
-    @patch('time.sleep', return_value=True)
-    def test_wait_for_db(self,ts):
+    @patch('time.sleep', return_value=None)
+    def test_wait_for_db(self, ts):
         """Test waiting for db"""
+
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
-            gi.side_effect = [OperationalError] * 5 + True
+            gi.side_effect = [OperationalError] * 5 + [True]
             call_command('wait_for_db')
-            self.assertEqual(gi.call_count,6)
+            self.assertEqual(gi.call_count, 6)
