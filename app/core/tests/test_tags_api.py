472a2f0e0b7d6c461d219372a3727c655c827f5b
Troy Stocks <tstocks@ceratopsdev.com>
2019-09-22 12:14:34 -0400

    Added create tag feature

--- app/core/tests/test_tags_api.py
+++ app/core/tests/test_tags_api.py
@@ -20,7 +20,6 @@ class PublicTagsApiTests(TestCase):
     def test_login_required(self):
         """Test that login is required for retrieving tags"""
         res = self.client.get(TAGS_URL)
-
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
@@ -61,3 +60,21 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tags_successful(self):
+        """Test creating a new tag"""
+        payload = {'name': 'Test tag'}
+        self.client.post(TAGS_URL, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_Create_tag_invalid(self):
+        """Test creating a new tag with invalid payload"""
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
